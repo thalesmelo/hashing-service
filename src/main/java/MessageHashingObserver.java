1c7f6c77192ed26d36b85dd70382fb3a8773f2ed
thalesmelo <thalesmelo@gmail.com>
2022-07-10 23:12:36 +0200

    Improved version

--- src/main/java/MessageHashingObserver.java
+++ src/main/java/MessageHashingObserver.java
@@ -1,11 +1,24 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class MessageHashingObserver implements Observer {
+
+	private static MessageDigest md;
+	static {
+		try {
+			// This is expensive so better reuse for processing of many messages
+			md = MessageDigest.getInstance("SHA-256"); // TODO: maybe switch for a more cryptographic strong algorithm.
+		} catch (NoSuchAlgorithmException e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
 
 	private static final int HASHING_ITERACTIONS = 5000;
 	private Sink sink;
 	private AtomicSalt saltProvider = new AtomicSalt();
+
+	private ReentrantLock lock = new ReentrantLock();
 
 	public MessageHashingObserver(Sink sink) {
 		this.sink = sink;
@@ -28,18 +41,17 @@ public class MessageHashingObserver implements Observer {
 		sink.publishHash(id, message, salt, hash(message, salt));
 	}
 
-	private synchronized byte[] hash(byte[] message, byte[] salt) {
-		MessageDigest md;
+	private byte[] hash(byte[] message, byte[] salt) {
+		lock.lock();
 		try {
-			md = MessageDigest.getInstance("SHA-256");
-		} catch (NoSuchAlgorithmException e) {
-			throw new IllegalArgumentException(e);
+			byte[] hashedMessage = message;
+			for (int i = 0; i < HASHING_ITERACTIONS; i++) {
+				hashedMessage = sha256(md, hashedMessage, salt);
+			}
+			return hashedMessage;
+		} finally {
+			lock.unlock();
 		}
-		byte[] hashedMessage = message;
-		for (int i = 0; i < HASHING_ITERACTIONS; i++) {
-			hashedMessage = sha256(md, hashedMessage, salt);
-		}
-		return hashedMessage;
 	}
 
 	private byte[] sha256(MessageDigest md, byte[] message, byte[] salt) {
@@ -48,6 +60,7 @@ public class MessageHashingObserver implements Observer {
 		return md.digest();
 	}
 
+	// TODO: Look for the fastest way to copy this array
 	private byte[] bytesConcat(byte[] message, byte[] salt) {
 		byte[] result = new byte[message.length + salt.length];
 		System.arraycopy(message, 0, result, 0, message.length);
